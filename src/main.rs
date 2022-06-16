ee7591c65078ede4b42bf49cd912d28860fe7a8f
Jenya705 <51133999+Jenya705@users.noreply.github.com>
2022-06-16 20:54:00 +0200

    For the next version of protocol server

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,135 @@
-use std::marker::PhantomData;
-use std::str::FromStr;
 use std::sync::Arc;
+use cubic_chat::color::DefaultColor;
 use cubic_chat::component::TextComponent;
-use cubic_protocol::server::server::{Connection, ProtocolServer, ProtocolServerHandler};
-use cubic_protocol::p1_18_2;
-use cubic_protocol::p1_18_2::{NextState, Pong, Response, ServerPacket, SHPacket, SSPacket};
-use cubic_protocol::protocol::ProtocolJson;
-use cubic_protocol::status::{StatusPlayers, StatusResponse, StatusSample, StatusVersion};
-use cubic_protocol::version::State;
-use log::{info, LevelFilter};
+use cubic_protocol::packet::PacketState;
+use cubic_protocol::packet_default::{ClientHandshakePacket, ClientLoginPacket, ClientStatusPacket, StatusPong, StatusResponse, StatusResponseDescription, StatusResponseObject, StatusResponsePlayers, StatusResponseVersion};
+use cubic_protocol_server::connection::Connection;
+use cubic_protocol_server::handler::{ConnectionHandler, ContainerReadHandler, PacketHandler, ReadHandler};
+use cubic_protocol_server::server::{ProtocolServerDeclare, run_server};
+use log::{LevelFilter, Record};
 use simple_logger::SimpleLogger;
-use uuid::Uuid;
 
-struct Handler {}
+struct MyConnectionHandler;
 
-impl ProtocolServerHandler<p1_18_2::ServerPacket> for Handler {
-    fn handle_connect(&self, connection: Arc<Connection>) {
-        info!("Client connected: {}", connection.get_addr());
+impl ConnectionHandler for MyConnectionHandler {
+    fn handle_connection(&self, connection: Arc<Connection>) {
+        println!("Connected: {}", connection.get_addr());
     }
 
     fn handle_disconnect(&self, connection: Arc<Connection>) {
-        info!("Client disconnected: {}", connection.get_addr());
+        println!("Disconnected: {}", connection.get_addr());
     }
+}
 
-    fn handle_event(&self, connection: Arc<Connection>, state: &mut State, packet: ServerPacket) {
-        info!("Received event: {:?}", packet);
+struct MyHandshakeReadHandler;
+
+struct MyStatusReadHandler;
+
+struct MyLoginReadHandler;
+
+struct MyPlayReadHandler;
+
+impl PacketHandler<ClientHandshakePacket> for MyHandshakeReadHandler {
+    fn handle_packet(
+        &self,
+        connection: Arc<Connection>,
+        state: &mut PacketState,
+        packet: ClientHandshakePacket,
+    ) {
+        println!("{:?}", packet);
         match packet {
-            ServerPacket::Handshake(packet) => match packet {
-                SHPacket::Handshaking(packet) => match packet.next_state {
-                    NextState::Status => *state = State::Status,
-                    NextState::Login => {
-                        tokio::spawn(async move { connection.close().await.unwrap() });
-                    }
-                }
-            }
-            ServerPacket::Status(packet) => match packet {
-                SSPacket::Request(request) => {
+            ClientHandshakePacket::Handshaking(handshaking) => match handshaking.next_state {
+                1 => *state = PacketState::Status,
+                2 => *state = PacketState::Login,
+                _ => {
                     tokio::spawn(async move {
-                        connection.write_object(&Response {
-                            response: ProtocolJson {
-                                value: StatusResponse {
-                                    version: StatusVersion {
-                                        protocol: 758,
-                                        name: "1.18.2".into(),
-                                    },
-                                    players: StatusPlayers {
-                                        max: 100,
-                                        online: 1,
-                                        sample: vec![
-                                            StatusSample {
-                                                name: "jenya705".into(),
-                                                id: Uuid::from_str("b31f5079-c09d-4979-b02e-2396e5fd9afb").unwrap(),
-                                            }
-                                        ],
-                                    },
-                                    description: TextComponent::new("hi!".into()).into(),
-                                    favicon: "".to_string(),
-                                }
-                            }
-                        }).await.unwrap()
+                        connection.close().await
                     });
                 },
-                SSPacket::Ping(packet) => {
-                    tokio::spawn(async move {
-                        connection.write_object(&Pong {
-                            payload: packet.payload,
-                        }).await.unwrap()
-                    });
-                }
             }
-            ServerPacket::Login(_) => {}
-            ServerPacket::Play(_) => {}
-        }
+        };
+    }
+}
+
+impl PacketHandler<ClientStatusPacket> for MyStatusReadHandler {
+    fn handle_packet(
+        &self,
+        connection: Arc<Connection>,
+        _: &mut PacketState,
+        packet: ClientStatusPacket
+    ) {
+        println!("{:?}", packet);
+        match packet {
+            ClientStatusPacket::StatusRequest(_) => tokio::spawn(async move {
+                connection.write_object(StatusResponse {
+                    response: StatusResponseObject {
+                        favicon: "".into(),
+                        players: StatusResponsePlayers {
+                            online: 5,
+                            max: 5,
+                            sample: Vec::new(),
+                        },
+                        description: StatusResponseDescription::Component({
+                            let mut component = TextComponent::new(
+                                "Cubic-Protocol Server!".into()
+                            );
+                            component.base.color = Some(DefaultColor::Red.into());
+                            component.base.bold = Some(true);
+                            component.into()
+                        }),
+                        version: StatusResponseVersion {
+                            protocol: 757,
+                            name: "1.18.2".into(),
+                        }
+                    }
+                }).await
+            }),
+            ClientStatusPacket::StatusPing(ping) => tokio::spawn(async move {
+                connection.write_object(StatusPong {
+                    payload: ping.payload
+                }).await
+            })
+        };
+    }
+}
+
+impl PacketHandler<ClientLoginPacket> for MyLoginReadHandler {
+    fn handle_packet(
+        &self,
+        connection: Arc<Connection>,
+        _: &mut PacketState,
+        packet: ClientLoginPacket
+    ) {
+        println!("{:?}", packet);
+        match packet {
+            ClientLoginPacket::LoginStart(start) => {
+                println!("Player {} trying to login", start.name);
+                tokio::spawn(async move { connection.close().await });
+            }
+            _ => {}
+        };
+    }
+}
+
+// it is doesn't matter
+impl PacketHandler<ClientStatusPacket> for MyPlayReadHandler {
+    fn handle_packet(&self, _: Arc<Connection>, _: &mut PacketState, _: ClientStatusPacket) {
+        unreachable!()
     }
 }
 
 #[tokio::main]
 async fn main() {
-    SimpleLogger::new().with_level(LevelFilter::Info).init().unwrap();
-    let server = ProtocolServer {
-        handler: Handler {},
-        host: "0.0.0.0:25565".to_string(),
-        packet_node: PhantomData,
-    };
-    server.run().await.unwrap()
+    SimpleLogger::new().with_level(LevelFilter::Debug).init();
+    let task = run_server(ProtocolServerDeclare {
+        host: "0.0.0.0:25565".into(),
+        connection_handler: MyConnectionHandler,
+        read_handler: ContainerReadHandler::new(
+            MyHandshakeReadHandler,
+            MyStatusReadHandler,
+            MyLoginReadHandler,
+            MyPlayReadHandler
+        ),
+    });
+    task.task.await.unwrap();
 }
